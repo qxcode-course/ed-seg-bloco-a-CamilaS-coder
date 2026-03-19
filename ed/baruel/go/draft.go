83bc3ba7ec2c58f7e88a24de2fe5cb89de31efea
Camila Santos <camilasantoss1302@gmail.com>
2026-03-19 18:33:54 -0300

    Atividade Baruel com map

--- ed/baruel/go/draft.go
+++ ed/baruel/go/draft.go
@@ -4,75 +4,126 @@ func main() {
 
     var figAlb, figBaruel int
     fmt.Scan(&figAlb, &figBaruel)
-    var i int
-    var j int
-    var k int
-    var apareceu bool
-    var figVet []int
-    primeiro := true
-    var contador int
-    var aux []int
-    //var aux2 []int
+    //valor := 0
+    figVet := make([]int, figBaruel)
+    unicas := make(map[int]bool)
+    repetidos := make([]int, 0, figBaruel)
     
-    for i = 0; i < figBaruel; i++{
-        var fig int
-        fmt.Scan(&fig)
-        figVet = append(figVet, fig)
-        
+    for i := range figVet {
+        fmt.Scan(&figVet[i])
     }
     
-    for i = 0; i < len(figVet); i++ {
-        apareceu = false
-        for  j = 0; j <= i - 1; j++{
-            if figVet[i] == figVet[j] {
-                apareceu = true
-                break
-            }
-            
-        } 
-            
-        if !apareceu{
-            contador := 0
+    for _, fig := range figVet {
 
-            for k = 0; k < len(figVet); k++{
-            if figVet[i] == figVet[k] {
-                //aux = append(aux, figVet[k])
-                 contador++
-                }
-            }
-            extras := contador - 1
-
-             for x := 0; x < extras; x++ {
-
-                if !primeiro {
-                    fmt.Print(" ")
-                }
-
-                fmt.Print(figVet[i])
-                primeiro = false
-            }
-
+        _, existe := unicas[fig]
+        if existe {
+            repetidos = append(repetidos, fig)
+        } else {
+            unicas[fig] = true
         }
-        
-        
+
     }
 
-    fmt.Println()
+    saida := fmt.Sprintf("%v", repetidos)
+    if saida == "[]" {
+        fmt.Println("N")
+    } else {
 
-    for y := 0; y < figAlb; y++ {
-        contador++
-        aux = append(aux, contador)
+        fmt.Println(saida[1: len(saida) - 1])
     }
 
-    //fmt.Print(aux)
+
+    faltantes := make([]int, 0, figAlb)
+
+    for i := 1; i <= figAlb; i++ {
+        if !unicas[i] {
+            faltantes = append(faltantes, i)
+        }
+    }
+
+    
+    sai := fmt.Sprintf("%v", faltantes)
+
+    if sai == "[]" {
+        fmt.Println("N")
+    } else {
+
+        fmt.Println(sai[1:len(sai) - 1])
+    }
 
 
+    
+
+    // var figAlb, figBaruel int
+    // fmt.Scan(&figAlb, &figBaruel)
+    // var i int
+    // var j int
+    // var k int
+    // var apareceu bool
+    // var figVet []int
+    // primeiro := true
+    // var contador int
+    // var aux []int
+    // //var aux2 []int
+    
+    // for i = 0; i < figBaruel; i++{
+    //     var fig int
+    //     fmt.Scan(&fig)
+    //     figVet = append(figVet, fig)
+        
+    // }
+    
     // for i = 0; i < len(figVet); i++ {
-    //     for j = i+1; j < len(figVet); j++ {
+    //     apareceu = false
+    //     for  j = 0; j <= i - 1; j++{
     //         if figVet[i] == figVet[j] {
-    //             aux = append(aux, figVet[i])
+    //             apareceu = true
+    //             break
+    //         }
+            
+    //     } 
+            
+    //     if !apareceu{
+    //         contador := 0
+
+    //         for k = 0; k < len(figVet); k++{
+    //         if figVet[i] == figVet[k] {
+    //             //aux = append(aux, figVet[k])
+    //              contador++
+    //             }
+    //         }
+    //         extras := contador - 1
+
+    //          for x := 0; x < extras; x++ {
+
+    //             if !primeiro {
+    //                 fmt.Print(" ")
+    //             }
+
+    //             fmt.Print(figVet[i])
+    //             primeiro = false
+    //         }
+
+    //     }
+        
+        
+    // }
+
+    // fmt.Println()
+
+    // for y := 0; y < figAlb; y++ {
+    //     contador++
+    //     aux = append(aux, contador)
+    // }
+
+    // for i := 0; i < len(figVet); i++ {
+    //     for j := 0; j < len(aux); j++ {
+    //         if figVet[i] != aux[j] {
+    //             fmt.Print(aux[j])
+    //             break
     //         }
     //     }
+
     // }
 
 
