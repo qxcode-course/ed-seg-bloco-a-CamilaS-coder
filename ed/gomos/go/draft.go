8a937ca1dc44059a9f380bf41b76458e7dc42d9d
Camila Santos <camilasantoss1302@gmail.com>
2026-03-22 19:42:25 -0300

    Atividade Gomos 22/03/2026

--- ed/gomos/go/draft.go
+++ ed/gomos/go/draft.go
@@ -1,5 +1,46 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("qxcode")
+    var Q int
+    var D string
+
+    fmt.Scan(&Q, &D)
+
+    x := make([]int, Q)
+    y := make([]int, Q)
+
+    for i := 0; i < Q; i++ {
+        fmt.Scan(&x[i], &y[i])
+    }
+
+    
+    oldX := make([]int, Q)
+    oldY := make([]int, Q)
+
+    copy(oldX, x)
+    copy(oldY, y)
+
+    
+    switch D {
+    case "L":
+        x[0]--
+    case "R":
+        x[0]++
+    case "U":
+        y[0]--
+    case "D":
+        y[0]++
+    }
+
+    
+    for i := 1; i < Q; i++ {
+        x[i] = oldX[i-1]
+        y[i] = oldY[i-1]
+    }
+
+    
+    for i := 0; i < Q; i++ {
+        fmt.Printf("%d %d\n", x[i], y[i])
+    }
+    //fmt.Println("qxcode")
 }
