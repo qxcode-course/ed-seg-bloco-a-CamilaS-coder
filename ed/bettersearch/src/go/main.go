edd6c86180ae72cf1fc4f2b54000f6972f597d77
Camila Santos <camilasantoss1302@gmail.com>
2026-05-26 10:57:59 -0300

    Atividade BetterSearch - Vetores - 26/05/26

---
+++ ed/bettersearch/src/go/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func BetterSearch(slice []int, value int) (bool, int) {
+
+	var low = 0;
+	var high = len(slice)
+	//var meio = 0
+
+	for low != high {
+		meio := low + ((high - low) / 2)
+
+		if slice[meio] == value {
+			return true, meio
+		}
+
+		if value < slice[meio] {
+			high = meio
+		}
+
+		if  value > slice[meio] {
+			low = meio + 1
+		}
+
+	}
+
+
+	return false, low
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	parts := strings.Split(scanner.Text(), " ")
+	slice := []int{}
+	for _, elem := range parts[1 : len(parts)-1] {
+		value, _ := strconv.Atoi(elem)
+		slice = append(slice, value)
+	}
+	scanner.Scan()
+	value, _ := strconv.Atoi(scanner.Text())
+	found, result := BetterSearch(slice, value)
+	if found {
+		fmt.Println("V", result)
+	} else {
+		fmt.Println("F", result)
+	}
+}
